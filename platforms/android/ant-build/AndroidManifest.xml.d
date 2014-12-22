9dba3f427f1dd6442b2a548b01d7f6079d2ad02d
akhileshrai <gibbet@gmail.com>
2014-12-22 19:41:45 +0530

    First Commit

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Akhilesh\Documents\GitHub\MyMediaExplorerOnsen\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Akhilesh\Documents\GitHub\MyMediaExplorerOnsen\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/MediaExplorerOnsenApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\Akhilesh\Documents\GitHub\MyMediaExplorerOnsen\platforms\android\ant-build\MediaExplorerOnsenApp-debug-unaligned.apk : \
+C:\Users\Akhilesh\Documents\GitHub\MyMediaExplorerOnsen\platforms\android\ant-build\MediaExplorerOnsenApp.ap_ \
+C:\Users\Akhilesh\Documents\GitHub\MyMediaExplorerOnsen\platforms\android\ant-build\classes.dex \
+C:\Users\Akhilesh\Documents\GitHub\MyMediaExplorerOnsen\platforms\android\CordovaLib\ant-build\classes.jar \
